99218383d500085f4d4db9f90c75e05289af5899
scheuerkev <scheuer.kevin@gmail.com>
2022-01-06 20:24:02 +0100

    Setting basic components logic

---
+++ src/components/movie-details/MovieDetails.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class MovieDetails extends Component {
+    render() {
+        return (
+            <div>MovieDetails</div>
+        );
+    }
+}
---
+++ src/components/movie-list/MovieElement.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class MovieElement extends Component {
+    render() {
+        return (
+            <div>MovieElement</div>
+        );
+    }
+}
