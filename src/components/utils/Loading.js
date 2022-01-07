addb2387c86114eb241c61967cbee5775f779e93
scheuerkev <scheuer.kevin@gmail.com>
2022-01-07 23:28:27 +0100

    Simulating data loadingz

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div style={{minHeight:'100vh'}}className="d-flex flex-row justify-content-center align-items-center w-100">
+            <img src="https://thumbs.gfycat.com/AmusedEnergeticDassie-max-1mb.gif" alt="Loading gif"/>
+        </div>
+    );
+}
