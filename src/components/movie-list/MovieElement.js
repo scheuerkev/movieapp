5e9e3bcd52bd0721930732715353b7be48add6c6
scheuerkev <scheuer.kevin@gmail.com>
2022-01-07 18:27:01 +0100

    Refactoring app

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -3,7 +3,16 @@ import React, {Component} from "react";
 export default class MovieElement extends Component {
     render() {
         return (
-            <div>MovieElement</div>
+            <div className="w-50 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" src="https://www.fnacspectacles.com/static/0/visuel/600/468/GLADIATOR-LYON_4683513286742310031.jpg"  />
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5>Titre</h5>
+                        <hr className="w-100"/>
+                        <p>Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire.</p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,33 +1,16 @@
 import React, {Component} from "react";
+import MovieElement from "./MovieElement";
 
 export default class MovieList extends Component {
     render() {
         return (
             <div className="w-75 d-flex flex-row flex-wrap align-content-start">
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
-                <div className="w-25 p-2">
-                    <div className="border">Film</div>
-                </div>
+                <MovieElement></MovieElement>
+                <MovieElement></MovieElement>
+                <MovieElement></MovieElement>
+                <MovieElement></MovieElement>
+                <MovieElement></MovieElement>
+                <MovieElement></MovieElement>
             </div>
         );
     }
